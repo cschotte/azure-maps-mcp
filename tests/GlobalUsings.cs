c699209d625766d713446a24a8c036d7cc0a244e
Clemens Schotte <clemens@navatron.com>
2025-08-09 20:45:13 +0200

    Remove test project and backup file from solution
    Deleted the 'azure-maps-mcp-tests' project and related test files, as well as a backup file for SearchTool.cs. The solution file was updated to exclude the test project.

--- tests/GlobalUsings.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Microsoft Corporation. All rights reserved.
-// Licensed under the MIT License.
-
-global using Xunit;
-global using FluentAssertions;
-global using Moq;
-global using System.Text.Json;
-global using Azure;
-global using Azure.Maps.Mcp.Tests.Helpers;
